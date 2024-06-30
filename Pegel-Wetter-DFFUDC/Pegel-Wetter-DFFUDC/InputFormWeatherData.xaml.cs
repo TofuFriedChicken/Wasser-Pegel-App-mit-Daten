1ff11642a6a150952557edc71c8d60840861945d
Lisa <lisamarie.bachmann@t-online.de>
2024-06-30 17:26:07 +0200

    create hidden input fields

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/InputFormWeatherData.xaml.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/InputFormWeatherData.xaml.cs
@@ -6,10 +6,27 @@ namespace Pegel_Wetter_DFFUDC
     {
       InitializeComponent();
 
-      
+
     }
 
-  
+    private void OnPickerSelectedIndexChanged(object sender, EventArgs e)     //source eventhandler https://learn.microsoft.com/de-de/dotnet/maui/user-interface/controls/picker?view=net-maui-8.0 (last visit: 30.06.24)
+    {
+      var picker = (Picker)sender;
+      int selectedIndex = picker.SelectedIndex;
+
+      if (selectedIndex != -1)
+      {
+        string selectedItem = (string)picker.ItemsSource[selectedIndex];
+
+        if (selectedItem == "Pegelstand") 
+        {
+          waterLevelForm.IsVisible = true;                      //source .IsVisible https://learn.microsoft.com/en-us/answers/questions/1160894/net-maui-how-to-show-or-hide-(-collapsed-)-content (last visit: 30.06.24)
+        }
+      }
+      
+
+    }
+
   }
 
 }
