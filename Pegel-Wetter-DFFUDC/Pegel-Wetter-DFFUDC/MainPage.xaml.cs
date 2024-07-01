cd88f6da931628697b93f785f8974b83e363f2c1
Johanna Junkert <s0591570@htw-berlin.de>
2024-07-01 15:28:38 +0200

    prototype circle 2

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MainPage.xaml.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MainPage.xaml.cs
@@ -6,15 +6,24 @@ namespace Pegel_Wetter_DFFUDC
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+            var location = new Location(52.5162, 13.3777);
+
+            MyMap_Test.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromMeters(1000)));
+
+            var circle = new Circle()
+            {
+                Radius = Distance.FromMeters(100000),
+                StrokeColor = Color.FromArgb("0000FF"),
+                StrokeWidth = 8,
+                FillColor = Color.FromArgb("ADD8E6")
+            };
+
+
 
             MyMap_Test.MapElements.Add(circle);
 
