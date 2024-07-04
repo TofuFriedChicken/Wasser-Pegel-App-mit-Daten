f60acf245c4a888492d8f22d5a0d3648e693a26c
Sophie Mena Chávez <sophie.menachavez@student.htw-berlin.de>
2024-07-04 18:48:12 +0200

    New Branche, right Nugets

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MauiProgram.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui.Maps;
+using Microsoft.Extensions.Logging;
 
 namespace Pegel_Wetter_DFFUDC
 {
@@ -9,6 +10,7 @@ namespace Pegel_Wetter_DFFUDC
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitMaps("beoVW2DRXJmNKDNXAU3a~q9N4iCClq08h7W7Z_LEFYA~AgM5IrnYZ6nogeA56gnZWzZqEOiCQWUEFGmWHr34e8fZhygfdT8EAEo44VQptTmE")
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
