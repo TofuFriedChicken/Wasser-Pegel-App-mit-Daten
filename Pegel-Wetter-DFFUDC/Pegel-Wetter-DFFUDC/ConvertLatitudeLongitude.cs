360ebbe8646750edc0644c949dbf57d2c6a73fd1
Sophie Mena Chávez <sophie.menachavez@student.htw-berlin.de>
2024-07-05 17:51:06 +0200

    push test 2

---
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/ConvertLatitudeLongitude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pegel_Wetter_DFFUDC
+{
+    class ConvertLatitudeLongitude
+    {
+        public object Convert(object value, Type type, object parameter, CultureInfo culture)
+        {
+            if (value is double latitude && parameter is double longitude)
+            {
+                return new Adress(latitude, longitude);
+            }
+            return new Adress(0, 0);
+        }
+
+        public object BackConvert(object value, Type type, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) //automatisch generiert
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
