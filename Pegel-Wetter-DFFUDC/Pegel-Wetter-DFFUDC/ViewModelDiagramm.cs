a318e1f51bc63528db6993bbd5834af1b7c4bb64
Colin Lorenzen <colin.lorenzen@student.htw-berlin.de>
2024-07-21 21:59:54 +0200

    DateTimePoint XAchse

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/ViewModelDiagramm.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/ViewModelDiagramm.cs
@@ -3,8 +3,7 @@ using LiveChartsCore;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
-
-
+using LiveChartsCore.Defaults;
 
 namespace Pegel_Wetter_DFFUDC
 {
@@ -13,6 +12,19 @@ namespace Pegel_Wetter_DFFUDC
         public ISeries[] Series { get; set; }
             = new ISeries[]
             {
+                /*new ColumnSeries<DateTimePoint>
+                {
+                    Values = new ObservableCollection<DateTimePoint>
+                    {
+                        new DateTimePoint(new DateTime(2021, 1, 1), 3),
+                        new DateTimePoint(new DateTime(2021, 1, 2), 6),
+                        new DateTimePoint(new DateTime(2021, 1, 3), 5),
+                        new DateTimePoint(new DateTime(2021, 1, 4), 3),
+                        new DateTimePoint(new DateTime(2021, 1, 5), 5),
+                        new DateTimePoint(new DateTime(2021, 1, 6), 8),
+                        new DateTimePoint(new DateTime(2021, 1, 7), 6)
+                    }
+                },*/ //example date scale from: https://livecharts.dev/docs/Maui/2.0.0-rc2/samples.axes.dateTimeScaled
                 new LineSeries<double>
                 {
                     Values = new double[] { 4, 6, 5, 3, -3, -1, 2 }
@@ -20,10 +32,11 @@ namespace Pegel_Wetter_DFFUDC
                 new ColumnSeries<double>
                 {
                     Values = new double[] { 2, 5, 4, -2, 4, -3, 5 }
-                }
+                } //example from: https://livecharts.dev/docs/Maui/2.0.0-rc2/Overview.Installation
             };
         public Axis[] XAxes { get; set; } =
         {
+            /*new DateTimeAxis(TimeSpan.FromDays(1), date => date.ToString("MMMM dd")),*/
             new Axis
             {
                 Name = "Datum",
@@ -36,12 +49,12 @@ namespace Pegel_Wetter_DFFUDC
             new Axis
             {
                 Name = "Niederschlag in mm / Pegelstand in m",
-                LabelsPaint = new SolidColorPaint
+                /*LabelsPaint = new SolidColorPaint
                 {
                     Color = SKColors.Blue,
                     FontFamily = "Times New Roman",
                     SKFontStyle = new SKFontStyle(SKFontStyleWeight.ExtraBold, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic)
-                },
+                },*/
             }
         };
     }
