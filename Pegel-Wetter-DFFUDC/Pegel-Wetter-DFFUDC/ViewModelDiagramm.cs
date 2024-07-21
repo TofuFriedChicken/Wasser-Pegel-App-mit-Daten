0d534808ca11d2483c994077011243fef65ec31b
Colin Lorenzen <colin.lorenzen@student.htw-berlin.de>
2024-07-21 23:15:27 +0200

    Niederschlagsdiagramm und Pegelstandsdiagramm vereinzelt

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/ViewModelDiagramm.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/ViewModelDiagramm.cs
@@ -9,7 +9,7 @@ namespace Pegel_Wetter_DFFUDC
 {
     public partial class ViewModelDiagramm : ObservableObject
     {
-        public ISeries[] Series { get; set; }
+        public ISeries[] Rainfall { get; set; }
             = new ISeries[]
             {
                 /*new ColumnSeries<DateTimePoint>
@@ -29,11 +29,31 @@ namespace Pegel_Wetter_DFFUDC
                 {
                     Values = new double[] { 4, 6, 5, 3, -3, -1, 2 }
                 },
-                new ColumnSeries<double>
-                {
-                    Values = new double[] { 2, 5, 4, -2, 4, -3, 5 }
-                } //example from: https://livecharts.dev/docs/Maui/2.0.0-rc2/Overview.Installation
+                //example from: https://livecharts.dev/docs/Maui/2.0.0-rc2/Overview.Installation
             };
+
+        public ISeries[] level { get; set; }
+            = new ISeries[]
+            {
+                /*new ColumnSeries<DateTimePoint>
+                {
+                    Values = new ObservableCollection<DateTimePoint>
+                    {
+                        new DateTimePoint(new DateTime(2021, 1, 1), 3),
+                        new DateTimePoint(new DateTime(2021, 1, 2), 6),
+                        new DateTimePoint(new DateTime(2021, 1, 3), 5),
+                        new DateTimePoint(new DateTime(2021, 1, 4), 3),
+                        new DateTimePoint(new DateTime(2021, 1, 5), 5),
+                        new DateTimePoint(new DateTime(2021, 1, 6), 8),
+                        new DateTimePoint(new DateTime(2021, 1, 7), 6)
+                    }
+                },*/ //example date scale from: https://livecharts.dev/docs/Maui/2.0.0-rc2/samples.axes.dateTimeScaled
+                new LineSeries<double>
+                {
+                    Values =  new double[] { 2, 5, 4, -2, 4, -3, 5 }
+                } 
+            };
+
         public Axis[] XAxes { get; set; } =
         {
             /*new DateTimeAxis(TimeSpan.FromDays(1), date => date.ToString("MMMM dd")),*/
@@ -44,11 +64,24 @@ namespace Pegel_Wetter_DFFUDC
                 LabelsRotation =15,
             }
         };
-        public Axis[] YAxes { get; set; } =
+        public Axis[] YAxesrainfall { get; set; } =
         {
             new Axis
             {
-                Name = "Niederschlag in mm / Pegelstand in m",
+                Name = "Niederschlag in mm",
+                /*LabelsPaint = new SolidColorPaint
+                {
+                    Color = SKColors.Blue,
+                    FontFamily = "Times New Roman",
+                    SKFontStyle = new SKFontStyle(SKFontStyleWeight.ExtraBold, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic)
+                },*/
+            }
+        };
+        public Axis[] YAxeslevel { get; set; } =
+        {
+            new Axis
+            {
+                Name = "Pegelstand in m",
                 /*LabelsPaint = new SolidColorPaint
                 {
                     Color = SKColors.Blue,
