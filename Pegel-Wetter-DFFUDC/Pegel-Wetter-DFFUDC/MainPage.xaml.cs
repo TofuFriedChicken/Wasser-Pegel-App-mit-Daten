52ece91f5fcca4b186f3b2c075e76e6e202b54fc
Johanna Junkert <s0591570@htw-berlin.de>
2024-07-22 16:39:57 +0200

    Inhalt von feature4.2 hinzugefügt da im Branch feature4.2/swapDates MauiProgram.cs Datei fehlt und wiederherstellen eines alten Zustandes nicht funktioniert hat

--- Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MainPage.xaml.cs
+++ Pegel-Wetter-DFFUDC/Pegel-Wetter-DFFUDC/MainPage.xaml.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
-using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.ApplicationModel;
 using System.Net.NetworkInformation;
 
+//Code behind
 namespace Pegel_Wetter_DFFUDC
 {
     public partial class MainPage : ContentPage
     {
+        private DateTime today = DateTime.Today;
+        Location location = new Location(52.5162, 13.3777); //current Berlin
 
         public MainPage()
         {
             InitializeComponent();
 
-            var location = new Location(52.5162, 13.3777);
+            //startpoint, based on variable location
+            MyMap_Test.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromMeters(1000)));
 
             MyMap_Test.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromMeters(1000)));
 
@@ -25,6 +28,8 @@ namespace Pegel_Wetter_DFFUDC
                 StrokeWidth = 8,
                 FillColor = Color.FromArgb("ADD8E6")
             };
+            
+            MyMap_Test.MapElements.Add(circle);
 
             Pin testpin = new Pin
             {
@@ -36,10 +41,76 @@ namespace Pegel_Wetter_DFFUDC
 
             MyMap_Test.Pins.Add(testpin);
 
-            MyMap_Test.MapElements.Add(circle);
+            UpdateTodayLabel(); //current date 
+        }
 
+        private void CurrentMapSizeUpdate(object sender, EventArgs e)
+        {
+            //update Mapsize based on current windowsize
+            MyMap_Test.WidthRequest = this.Width;
+            MyMap_Test.HeightRequest = this.Height;
+        }
+
+        private void UpdateTodayLabel()
+        {
+            DateLabel.Text = today.ToString("dddd, dd. MMMM yyyy"); //form changeable
+        }
+
+        private void DateBack_Clicked(object sender, EventArgs e)
+        {
+            today = today.AddDays(-1); //1 day to past
+            UpdateTodayLabel();
+        }
+
+        private async void DateForward_Clicked(object sender, EventArgs e)
+        {
+            //shouldn't see/click to future dates
+            if (today == DateTime.Today)
+            {
+                string currentDayPopUp = "You've reached the current date.";
+                await DisplayAlert("Information", currentDayPopUp, "Exit");
+            }
+            else
+            {
+                today = today.AddDays(1); //1 day to future
+                UpdateTodayLabel();
+            }
+
+        }
+
+        //getWaterlevel in Listenform (von Sophie feature 1.1)
+        private void AddPinsWaterlevel()
+        {
+            MyMap_Test.Pins.Clear();
+
+            //datum auslesen und nur tagesaktuelle daten in detailfenster anzeigen bei mouseover
+
+            /*
+            foreach (var item in collection)
+            {
+                Pins erzeugen
+            }*/
+        }
+
+        private void rainfallmap_Clicked(object sender, EventArgs e)
+        {
+            //aktuelles datum abfragen und aktualisieren
+            AddPinsWaterlevel();
+        }
+
+        //getWaterlevel in Listenform (von Sophie feature 1.1)
+        private void AddPinsRainfall()
+        {
+            MyMap_Test.Pins.Clear();
+        }
+
+        private void waterlevelmap_Clicked(object sender, EventArgs e)
+        {
+            AddPinsRainfall();
         }
 
     }
 
 }
+
+
